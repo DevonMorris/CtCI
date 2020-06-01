0b146c2482512fc8d1754e1f265fc5c10b3b7267
Devon Morris <devonmorris1992@gmail.com>
2020-06-01 14:16:40 +0000

    remove node

---
+++ exercises/lists/2-3.cpp
@@ -0,0 +1,10 @@
+// Exercise 2.3 from CtCI p.94
+// Remove an element
+// Timothy Devon Morris
+// Mon Jun  1 14:00:30 UTC 2020
+
+/**
+ * BCR is O(1)
+ * I'm not implementing my own List DS, so I have no way to do this problem
+ * The crux of the algorithm is to copy the next node into the current node and delete the next node. This will not work if the current node is the last node in the list.
+ */
